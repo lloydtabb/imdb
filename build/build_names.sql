acb9e619f12a5713bda4ffb3bda13fb95ed0c7e2
lloydtabb <ltabb@google.com>
2022-09-14 15:04:43 +0000

    fix birth year

--- build/build_names.sql
+++ build/build_names.sql
@@ -1,6 +1,10 @@
 copy (
   SELECT
-    n.*
+    n.nconst,
+    n.primaryName,
+    n.primaryProfession,
+    case WHEN regexp_matches(birthYear,'[0-9]+') THEN CAST(birthYear as integer) END as birthYear,
+    case WHEN regexp_matches(deathYear,'[0-9]+') THEN CAST(deathYear as integer) END as deathYear,
   FROM read_csv_auto('name.basics.tsv.gz', delim='\t', quote='',header=True, all_varchar=true) as n
   	INNER JOIN '../docs/principals.parquet' as p ON n.nconst = p.nconst
 ) to '../docs/names.parquet' (FORMAT 'parquet', CODEC 'ZSTD')
