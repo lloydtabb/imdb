9f4dbf4c93447c21ed11972be60886af9422563c
lloydtabb <ltabb@google.com>
2022-09-23 16:17:12 +0000

    update names

--- build_names.sql
+++ build_names.sql
@@ -7,4 +7,5 @@ copy (
     case WHEN regexp_matches(deathYear,'[0-9]+') THEN CAST(deathYear as integer) END as deathYear,
   FROM read_csv_auto('name.basics.tsv.gz', delim='\t', quote='',header=True, all_varchar=true) as n
   	INNER JOIN 'principals.parquet' as p ON n.nconst = p.nconst
+  GROUP BY 1,2,3,4,5
 ) to 'names.parquet' (FORMAT 'parquet', CODEC 'ZSTD')
