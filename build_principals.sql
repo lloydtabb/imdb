c5f1e5d22294fda53739ec527b2e03070d5ac350
lloydtabb <ltabb@google.com>
2022-10-13 17:44:40 +0000

    build composer demo:

--- build_principals.sql
+++ build_principals.sql
@@ -2,7 +2,9 @@
 copy (
 	SELECT
 		p.tconst, ordering, nconst, category, job,
-		str_split(characters,',') as characters
+		str_split(
+			regexp_replace(characters,'[\[\]\"]','','g'),
+			',') as characters
 	FROM read_csv_auto('title.principals.tsv.gz', delim='\t', quote='',header=True) as p
 	INNER JOIN 'titles.parquet' as t ON t.tconst = p.tconst
 ) to 'principals.parquet' (FORMAT 'parquet', CODEC 'ZSTD')
