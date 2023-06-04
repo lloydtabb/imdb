7f374917c553aed233e7f3772d5cf6a42ff82b46
lloyd tabb <ltabb@google.com>
2023-06-04 12:26:51 -0700

    attempt at converting to malloysql

---
+++ data/get.sh
@@ -0,0 +1,5 @@
+wget \
+  https://datasets.imdbws.com/title.crew.tsv.gz \
+  https://datasets.imdbws.com/title.ratings.tsv.gz \
+  https://datasets.imdbws.com/title.basics.tsv.gz \
+  https://datasets.imdbws.com/name.basics.tsv.gz 
