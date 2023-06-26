0efc866f7174b9d887095c5e4bd45a71f21da5ae
lloyd tabb <ltabb@google.com>
2023-06-26 08:25:18 -0500

    Convert transformation pipeline to Malloy (#5)
    * bustage
    
    * Brand new ETL
    
    * spaces
    
    * fix indentation
    
    * add some comments
    
    * update titles
    
    * make fetch from internet
    
    * remove parens

--- data/get.sh
+++ data/get.sh
@@ -2,4 +2,5 @@ wget \
   https://datasets.imdbws.com/title.crew.tsv.gz \
   https://datasets.imdbws.com/title.ratings.tsv.gz \
   https://datasets.imdbws.com/title.basics.tsv.gz \
+  https://datasets.imdbws.com/title.principals.tsv.gz \
   https://datasets.imdbws.com/name.basics.tsv.gz 
